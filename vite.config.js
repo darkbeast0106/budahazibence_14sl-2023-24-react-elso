314ae4468767bcb74fb784bd335842405d78b583
Budaházi Bence <budahazi.bence@petrik.hu>
2024-02-08 17:16:33 +0100

    GIthub pages base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/budahazibence_14sl-2023-24-react-elso/",
   plugins: [react()],
 })
