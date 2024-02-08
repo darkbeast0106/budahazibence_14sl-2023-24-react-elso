77e2fa01db2da03b9918e986ca0ee19277bd54c6
Budaházi Bence <budahazi.bence@petrik.hu>
2024-02-08 16:38:49 +0100

    Content komponens

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,24 @@
-function Content() {
-    return ( <></> );
+import "./Content.css"
+import PropTypes from "prop-types";
+
+function Content(props) {
+    const { count } = props;
+    const paragraphs = [];
+    for (let i = 0; i < count; i++) {
+        paragraphs.push(<p key={i}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos beatae tempora commodi vel unde facere itaque provident atque molestias minima odit iusto repudiandae voluptate at magni, et quia quis distinctio!</p>);
+    }
+
+    return ( <div id="content" className="first-content">
+        {paragraphs}
+    </div> );
+}
+
+Content.propTypes = {
+    count: PropTypes.number
+}
+
+Content.defaultProps = {
+    count: 5
 }
 
 export default Content;
