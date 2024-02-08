9dc9d8fa46bfac13093b2d1d0263ee5567eaa8a9
Budaházi Bence <budahazi.bence@petrik.hu>
2024-02-08 16:49:43 +0100

    Footer komponens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,19 @@
-function Footer() {
-    return ( <></> );
+import "./Footer.css";
+import PropTypes from "prop-types";
+
+function Footer(props) {
+  const { name, link } = props;
+
+  return (
+    <footer className="first-footer">
+      <p>Készítette: {name} – elérhető <a href={link}>itt</a>.</p>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+    name: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired
 }
 
 export default Footer;
