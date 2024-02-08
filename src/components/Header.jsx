bdce75765c2b781e0e9c0a8ad7159d7d98646b75
Budaházi Bence <budahazi.bence@petrik.hu>
2024-02-08 16:30:43 +0100

    Üres komponensek létrehozása, header komponens befejezése

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="first-header">
+      <h1>Első react alkalmazás</h1>
+    </header>
+  );
+}
+
+export default Header;
